c3c8a01f6971988f24f00db4fe5567db87415e68
Kuerm <kuerm@gmx.net>
2025-12-16 10:53:35 +0100

    f: step back

--- src/main/java/ch/css/m3000/MarsRover.java
+++ src/main/java/ch/css/m3000/MarsRover.java
@@ -20,7 +20,8 @@ public class MarsRover {
     public MarsRover move(final String commands) {
         var position1 = this.currentPosition;
         for (var c : commands.chars().toArray()) {
-            position1 = new Position(position1.x(), position1.y() + 1);
+            int step = c == 'f' ? 1 : -1;
+            position1 = new Position(position1.x(), position1.y() + step);
         }
 
         return new MarsRover(position1, currentDirection);
