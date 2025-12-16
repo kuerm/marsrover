6861f7e70c3a5a3f213486cfe8a6de96345db09a
Kuerm <kuerm@gmx.net>
2025-12-16 10:56:41 +0100

    r: use record power

--- src/main/java/ch/css/m3000/MarsRover.java
+++ src/main/java/ch/css/m3000/MarsRover.java
@@ -1,30 +1,20 @@
 package ch.css.m3000;
 
-public class MarsRover {
-    private final Direction currentDirection;
-    private final Position currentPosition;
+public record MarsRover(Position position, Direction direction) {
+    private static final char FORWARD_STEP = 'f';
 
-    public MarsRover(final Position position, final Direction direction) {
-        this.currentPosition = position;
-        this.currentDirection = direction;
-    }
-
-    public Direction direction() {
-        return currentDirection;
-    }
-
-    public Position position() {
-        return currentPosition;
+    private static boolean isForwardCommand(final int c) {
+        return c == FORWARD_STEP;
     }
 
     public MarsRover move(final String commands) {
-        var position1 = this.currentPosition;
-        for (var c : commands.chars().toArray()) {
-            int step = c == 'f' ? 1 : -1;
+        var position1 = this.position;
+        for (var command : commands.chars().toArray()) {
+            int step = isForwardCommand(command) ? 1 : -1;
             position1 = new Position(position1.x(), position1.y() + step);
         }
 
-        return new MarsRover(position1, currentDirection);
+        return new MarsRover(position1, direction);
 
     }
 }
